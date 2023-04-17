48583d889e10a94a1b16252ba825bb494bbd6c47
Lil s0uxz <gkh.0546@gmail.com>
2023-04-17 08:57:34 +0700

    24

--- Updater/Home.Designer.cs
+++ Updater/Home.Designer.cs
@@ -89,7 +89,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(6, 19);
+            this.button1.Location = new System.Drawing.Point(484, 19);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(86, 68);
             this.button1.TabIndex = 2;
